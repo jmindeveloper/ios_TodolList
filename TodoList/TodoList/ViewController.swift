269f50ce726ec5d0a3900ec0614e7100c8c55a0c
jmindevelper <jmin.developer@gmail.com>
2021-08-04 13:22:31 +0900

    new project

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by J_Min on 2021/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
